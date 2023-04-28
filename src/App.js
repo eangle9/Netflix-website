eb2faecde8fcc2008ab590fddf6fd362ccff2119
engdawork <engdaworkyismaw9@gmail.com>
2023-04-28 10:12:14 +0300

    loginScreen logo and background image added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import './App.css';
-import Banner from './components/Banner/Banner';
-//import HomeScreen from './pages/HomeScreen/HomeScreen';
-import Navbar from './components/Navbar/Navbar';
-import Row from './components/Row/Row';
-import Requests from './components/Requests';
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import HomeScreen from './pages/HomeScreen/HomeScreen';
+import LoginScreen from './pages/login screen/LoginScreen';
+
+
 
 
 
 function App() {
+  const router = createBrowserRouter([
+      {
+        path: "/",
+        element: <HomeScreen/>,
+      },
+      {
+        path:"/login",
+        element: <LoginScreen/>,
+      }
+  ]);
+
+
   return (
     <div className="app">
-    {/* <HomeScreen/> */}
-    <Navbar/>
-    <Banner/>
-    <Row title="NETFLIX ORIGINAL" fetchUrl={Requests.fetchNetflixOriginals} isLargeRow />
-    <Row title="Trending Now" fetchUrl={ Requests.fetchTrending} />
-    <Row title="Top Rated" fetchUrl={ Requests.fetchTopRated } />
-    <Row title="Action Movies" fetchUrl={Requests.fetchActionMovies} />
-    <Row title="Comedy Movies" fetchUrl={ Requests.fetchComedyMovies } />
-    <Row title="Horror Movies" fetchUrl={ Requests.fetchHorrorMovies } />
-    <Row title="Romance Movies" fetchUrl={ Requests.fetchRomanceMovies } />
-    <Row title="Documentaries" fetchUrl={ Requests.fetchDocumentaries } />
+        <RouterProvider router={router} />
     </div>
   );
 }
--- src/pages/HomeScreen/HomeScreen.js
+++ src/pages/HomeScreen/HomeScreen.js
@@ -1,11 +1,26 @@
 import React from "react";
-import "../HomeScreen/HomeScreen.css";
-import Navbar from "./"
+import "./HomeScreen.css";
+import Navbar from "../../components/Navbar/Navbar";
+import Row from "../../components/Row/Row";
+import Requests from "../../components/Requests";
+import Banner from "../../components/Banner/Banner";
+
 
 const HomeScreen = () => {
-    return(
+    return (
         <>
-        <Navbar/>
+            <div className="home-screen">
+                <Navbar />
+                <Banner />
+                <Row title="NETFLIX ORIGINAL" fetchUrl={Requests.fetchNetflixOriginals} isLargeRow />
+                <Row title="Trending Now" fetchUrl={Requests.fetchTrending} />
+                <Row title="Top Rated" fetchUrl={Requests.fetchTopRated} />
+                <Row title="Action Movies" fetchUrl={Requests.fetchActionMovies} />
+                <Row title="Comedy Movies" fetchUrl={Requests.fetchComedyMovies} />
+                <Row title="Horror Movies" fetchUrl={Requests.fetchHorrorMovies} />
+                <Row title="Romance Movies" fetchUrl={Requests.fetchRomanceMovies} />
+                <Row title="Documentaries" fetchUrl={Requests.fetchDocumentaries} />
+            </div>
         </>
     );
 }
